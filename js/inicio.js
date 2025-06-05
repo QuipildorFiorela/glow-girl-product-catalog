c9e03c86d05665b63bba1ef527336eae75d1b0ce
Fiorela <159023986+QuipildorFiorela@users.noreply.github.com>
2025-06-05 18:12:00 -0300

    Inicio arreglado
    El inicio ya cuenta con el JS y la estética arreglada

---
+++ js/inicio.js
@@ -0,0 +1,26 @@
+
+function funcionalidadBtnIngresar(){
+    document.addEventListener("DOMContentLoaded", () => {
+    const btnIngresar = document.getElementById("btn-ingresar");
+    const inputNombre = document.getElementById("nombre");
+
+    btnIngresar.addEventListener("click", () => {
+        const nombre = inputNombre.value.trim();
+
+        if (nombre) {
+        // Guardamos el nombre si querés usarlo en otra página
+        localStorage.setItem("nombreUsuario", nombre);
+        // Redirigimos a la página principal
+        window.location.href = "index.html";
+        } else {
+        alert("Por favor ingresá tu nombre.");
+        }
+    });
+    });
+}
+
+async function init() {
+    funcionalidadBtnIngresar();
+}
+
+init();
