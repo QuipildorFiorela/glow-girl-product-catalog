7ef8a0b2d0c72b1a34b8cc9b2cfc0be67170e181
Fiorela <159023986+QuipildorFiorela@users.noreply.github.com>
2025-06-09 17:55:31 -0300

    darkMode ahora sí, me olvidé

--- js/acceso.js
+++ js/acceso.js
@@ -1,17 +1,28 @@
 
 // Modo oscuro
 function darkMode() {
+    const btnMode = document.getElementById("btn-mode");
     const logoTienda = document.getElementById("logo-tienda");
-    const btnColorMode = document.getElementById("btn-mode")
-    btnColorMode.addEventListener("click", () => {
-        document.body.classList.toggle("modo-oscuro"); //Toggle: interruptor, si no tiene la clase "modo-oscuro" la crea, sino la elimina. La primera vez q toco el boton se pasa a crear la clase
 
-        // Cambiar imagen del botón
-        const modoOscuro = document.body.classList.contains("modo-oscuro"); //verifico si el body tiene la clase modo oscuro, guarda true o false
-        btnColorMode.src = modoOscuro ? "./img/icons/dark_mode_icon.png" : "./img/icons/light_mode_icon.png"; //si es true modo oscuro : sino modo light
-        logoTienda.src = modoOscuro ? "./img/icons/logo_tienda_dark_icon.png" : "./img/icons/logo_tienda_light_icon.png";
+    // Verificar si ya había un modo guardado
+    if (localStorage.getItem("modo") === "oscuro") {
+        document.body.classList.add("modo-oscuro");
+        btnMode.src = "./img/icons/dark_mode_icon.png";
+        logoTienda.src= "./img/icons/logo_tienda_dark_icon.png";
+    }
+
+    btnMode.addEventListener("click", () => {
+        const modoActivo = document.body.classList.toggle("modo-oscuro");
+        
+        // Cambiar ícono
+        btnMode.src = modoActivo ? "./img/icons/dark_mode_icon.png" : "./img/icons/light_mode_icon.png";
+        logoTienda.src = modoActivo ? "./img/icons/logo_tienda_dark_icon.png" : "./img/icons/logo_tienda_light_icon.png";
+
+        // Guardar en localStorage
+        localStorage.setItem("modo", modoActivo ? "oscuro" : "claro");
     });
 }
+
 
 function funcionalidadBtnIngresar(){
     document.addEventListener("DOMContentLoaded", () => {
