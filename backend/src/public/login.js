632ceeaa8f19f5f872e1b1a04f08cbd66b953873
Bianca Monteleone <biancamonteleone16@gmail.com>
2025-06-26 17:46:01 -0300

    boton admin
    *Se agregó un boton en acceso que redirige a products.ejs
    *Comienzo de login.ejs

---
+++ backend/src/public/login.js
@@ -0,0 +1,52 @@
+
+// Modo oscuro
+function darkMode() {
+    const btnMode = document.getElementById("btn-mode");
+    const logoTienda = document.getElementById("logo-tienda");
+
+    // Verificar si ya había un modo guardado
+    if (localStorage.getItem("modo") === "oscuro") {
+        document.body.classList.add("modo-oscuro");
+        btnMode.src = "http://localhost:5000/img/icons/dark_mode_icon.png";
+        logoTienda.src= "http://localhost:5000/img/icons/logo_tienda_dark_icon.png";
+    }
+
+    btnMode.addEventListener("click", () => {
+        const modoActivo = document.body.classList.toggle("modo-oscuro");
+        
+        // Cambiar ícono
+        btnMode.src = modoActivo ? "http://localhost:5000/img/icons/dark_mode_icon.png" : "http://localhost:5000/img/icons/light_mode_icon.png";
+        logoTienda.src = modoActivo ? "http://localhost:5000/img/icons/logo_tienda_dark_icon.png" : "http://localhost:5000/img/icons/logo_tienda_light_icon.png";
+
+        // Guardar en localStorage
+        localStorage.setItem("modo", modoActivo ? "oscuro" : "claro");
+    });
+}
+
+
+function funcionalidadBtnIngresar(){
+    document.addEventListener("DOMContentLoaded", () => {
+    const btnIngresar = document.getElementById("btn-ingresar");
+    const inputNombre = document.getElementById("nombre");
+
+    btnIngresar.addEventListener("click", () => {
+        const nombre = inputNombre.value.trim();
+
+        if (nombre) {
+        //guardamos el nombre para usarlo en otra página
+        localStorage.setItem("nombreUsuario", nombre);
+        //redirigimos a la página principal
+        window.location.href = "inicio.html";
+        } else {
+        alert("Por favor ingresá tu nombre.");
+        }
+    });
+    });
+}
+
+async function init() {
+    darkMode();
+    funcionalidadBtnIngresar();
+}
+
+init();
